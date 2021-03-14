f6ae6bc47b937d1168ae0159a133a6a4ffd76406
Azhar Basit <azhar.basit@gmail.com>
2021-03-15 03:40:04 +0900

    adding integration test for adding and getting records

---
+++ tests/integration/test_get_records.py
@@ -0,0 +1,13 @@
+import requests
+
+BASE = "http://127.0.0.1:5000"
+
+def test_get_record():
+    response = requests.get(BASE+"/job/"+str(1))
+    assert response.status_code == 200
+    assert response.json()  == {"id": 1, "name": "job1","created_at" : '2015-06-05T08:10:10.000010',"status" : "started"}
+
+def test_get_missing_record():
+    response = requests.get(BASE+"/job/"+str(10))
+    assert response.status_code == 404
+    assert response.json() == {"message":"The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again."}
